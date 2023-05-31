71c571852642075634da9fa6ea98cf6b10bb1232
Pain4Din0 <liyuu@outlook.jp>
2023-05-31 20:13:30 +0800

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,8 +1,3 @@
-########################################
-# FOR USAGE: RUN BELOW COMMAND         #
-# python scan.py -i images/m1.jpg #
-########################################
-#From other file to make a rectangle after finding four points
 from pyimagesearch.transform import four_point_transform
 
 import numpy as np
@@ -10,77 +5,51 @@ import argparse
 import cv2
 import imutils
 
-### construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True,
 	help = "Path to the image to be scanned")
 args = vars(ap.parse_args())
 
-#loading image
+# 加载图像，计算图像比例
 image = cv2.imread(args["image"])
 
-# Compute the ratio of the old height to the new height, clone it, 
-# and resize it easier for compute and viewing
+# 调整图像尺寸
 ratio = image.shape[0] / 500.0
 orig = image.copy()
 image = imutils.resize(image, height = 500)
 
-### convert the image to grayscale, blur it, and find edges in the image
-
+# 灰阶处理
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Gaussian Blurring to remove high frequency noise helping in
-# Contour Detection 
+# 高斯模糊
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
-# Canny Edge Detection
+# Canny
 edged = cv2.Canny(gray, 75, 200)
 
-
 print("STEP 1: Edge Detection")
-# cv2.imshow("Image", image)
+cv2.imshow("Original", image)
 cv2.imshow("Edged", edged)
 
-# finding the contours in the edged image, keeping only the
-# largest ones, and initialize the screen contour
+# 寻找边缘图像中的轮廓，只保留最大的轮廓
 cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-## What are Contours ?
-## Contours can be explained simply as a curve joining all the continuous
-## points (along the boundary), having same color or intensity. 
-## The contours are a useful tool for shape analysis and object detection 
-## and recognition.
-
-# Handling due to different version of OpenCV
+# OpenCV 版本兼容性处理
 cnts = cnts[1] if imutils.is_cv3() else cnts[0]
 
-# Taking only the top 5 contours by Area
+# 只取面积最大的5个轮廓
 cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
-### Heuristic & Assumption
-
-# A document scanner simply scans in a piece of paper.
-# A piece of paper is assumed to be a rectangle.
-# And a rectangle has four edges.
-# Therefore use a heuristic like : we’ll assume that the largest
-# contour in the image with exactly four points is our piece of paper to 
-# be scanned.
-
-# looping over the contours
 for c in cnts:
-	### Approximating the contour
-
-	#Calculates a contour perimeter or a curve length
+	# 计算轮廓线周长或曲线长度
 	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.01 * peri, True)#0.02
-
-	# if our approximated contour has four points, then we
-	# can assume that we have found our screen
+	approx = cv2.approxPolyDP(c, 0.01 * peri, True)
+	# 当找到四个顶角时
 	screenCnt = approx
 	if len(approx) == 4:
 		screenCnt = approx
 		break
 	
-# show the contour (outline) 
+# 展示
 print("STEP 2: Finding Boundary")
 
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
